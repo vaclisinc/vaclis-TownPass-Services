cf429101031ffdd0e538c1239168b3fbfc0f0237
nyraa <112930946+nyraa@users.noreply.github.com>
2024-09-08 01:38:03 +0800

    update event

--- src/views/ParkingService.vue
+++ src/views/ParkingService.vue
@@ -1,64 +1,75 @@
 <script setup lang="ts">
 import Map from '@/components/organisms/Map.vue';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import NavigatorCard from "@/components/organisms/NavigatorCard.vue";
+import NavigatorCard from '@/components/organisms/NavigatorCard.vue';
 import { ref } from 'vue';
 import { computed } from 'vue';
 
 export type ParkPoint = {
-    name: string;
-    address: string;
-    lat: number;
-    lng: number;
-    remainingSpace: number;
-    price: string;
-    distance: number;
-    type: 'park' | 'yellow_line';
+  name: string;
+  address: string;
+  lat: number;
+  lng: number;
+  remainingSpace: number;
+  price: string;
+  distance: number;
+  type: 'park' | 'yellow_line';
 };
 
 enum NavigatorStep {
-    NavigatorPark = 'navigator_park',
-    NavigatorYellowLine = 'navigator_yellow_line',
-    ParkConfirm = 'park_confirm',
-    ParkTimer = 'park_timer',
-};
+  NavigatorPark = 'navigator_park',
+  NavigatorYellowLine = 'navigator_yellow_line',
+  ParkConfirm = 'park_confirm',
+  ParkTimer = 'park_timer'
+}
 
 const currentSelectedPark = ref<ParkPoint | null>({
-    name: 'sussy',
-    address: 'safa',
-    lat: 100,
-    lng: 24,
-    remainingSpace: 48763,
-    price: '40圓/小時',
-    distance: 20,
-    type: 'park'
+  name: 'sussy',
+  address: 'safa',
+  lat: 100,
+  lng: 24,
+  remainingSpace: 48763,
+  price: '40圓/小時',
+  distance: 20,
+  type: 'park'
 });
 const currentStep = ref<NavigatorStep>(NavigatorStep.NavigatorYellowLine);
 const handleClick = (point: ParkPoint) => {
-    currentSelectedPark.value = point;
+  currentSelectedPark.value = point;
+};
+const handleGoClick = () => {
+  // TODO: pop up the routing card
+};
+const handleBackClick = () => {
+  currentSelectedPark.value = null;
 };
 
-
+const backClickHandler = ref(handleBackClick);
+const goClickHandler = ref(handleGoClick);
 const pointClickHandler = ref(handleClick);
 const isShowNavigatorCard = computed(() => currentSelectedPark.value !== null);
 
 // Usage: <Map @point-click="pointClickHandler" />
 </script>
 <template>
-    <div class="h-screen flex flex-col overflow-hidden position-relative">
-        <div class="flex flex-1">
-            <Map @point-click="pointClickHandler" />
-        </div>
-        <NavigatorCard v-show="isShowNavigatorCard"
-            :parkName="currentSelectedPark?.name ?? null"
-            :remainingSpace="currentSelectedPark?.remainingSpace ?? null"
-            :price="currentSelectedPark?.price ?? null"
-            :distance="currentSelectedPark?.distance ?? null"
-            :address="currentSelectedPark?.address ?? null"
-            :display="currentStep"
-            :timePassed="null"
-            :maxTime="null"
-            :leaveEarly="true"
-            show />
+  <div class="h-screen flex flex-col overflow-hidden position-relative">
+    <div class="flex flex-1">
+      <Map @point-click="pointClickHandler" />
     </div>
+    <NavigatorCard
+      v-show="isShowNavigatorCard"
+      :parkName="currentSelectedPark?.name ?? null"
+      :remainingSpace="currentSelectedPark?.remainingSpace ?? null"
+      :price="currentSelectedPark?.price ?? null"
+      :distance="currentSelectedPark?.distance ?? null"
+      :address="currentSelectedPark?.address ?? null"
+      :display="currentStep"
+      :timePassed="null"
+      :maxTime="null"
+      :leaveEarly="true"
+      :show="isShowNavigatorCard"
+      @button-back="backClickHandler"
+      @button-go="goClickHandler"
+    />
+  </div>
 </template>
