6b958fa2a1ae3502eac6324a70d1024616219c9d
SimonLiu <simonliu423@gmail.com>
2024-09-08 02:11:48 +0800

    Feat: add notification test to app

--- src/views/ParkingService.vue
+++ src/views/ParkingService.vue
@@ -1,36 +1,41 @@
 <script setup lang="ts">
 import Map from '@/components/organisms/Map.vue';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import NavigatorCard from "@/components/organisms/NavigatorCard.vue";
+import NavigatorCard from '@/components/organisms/NavigatorCard.vue';
+import { useConnectionMessage } from '@/composables/useConnectionMessage';
+import BaseButton from '@/components/atoms/BaseButton.vue';
 import { ref } from 'vue';
 import { computed } from 'vue';
 
 export type ParkPoint = {
-    name: string;
-    address: string;
-    lat: number;
-    lng: number;
-    remainingSpace: number;
-    price: string;
-    distance: number;
-    display: "navigator_park" | "navigator_yellow_line" | "park_confirm" | "park_timer";
+  name: string;
+  address: string;
+  lat: number;
+  lng: number;
+  remainingSpace: number;
+  price: string;
+  distance: number;
+  display: 'navigator_park' | 'navigator_yellow_line' | 'park_confirm' | 'park_timer';
 };
 
 const currentSelectedPark = ref<ParkPoint | null>({
-    name: 'sussy',
-    address: 'safa',
-    lat: 100,
-    lng: 24,
-    remainingSpace: 48763,
-    price: '40圓/小時',
-    distance: 20,
-    display: 'navigator_park'
+  name: 'sussy',
+  address: 'safa',
+  lat: 100,
+  lng: 24,
+  remainingSpace: 48763,
+  price: '40圓/小時',
+  distance: 20,
+  display: 'navigator_park'
 });
 
 const handleClick = (point: ParkPoint) => {
-    currentSelectedPark.value = point;
+  currentSelectedPark.value = point;
 };
 
+const handleTestClick = () => {
+  useConnectionMessage('notification', 'test');
+};
 
 const pointClickHandler = ref(handleClick);
 const isShowNavigatorCard = computed(() => currentSelectedPark.value !== null);
@@ -38,20 +43,23 @@ const isShowNavigatorCard = computed(() => currentSelectedPark.value !== null);
 // Usage: <Map @point-click="pointClickHandler" />
 </script>
 <template>
-    <div class="h-screen flex flex-col overflow-hidden position-relative">
-        <div class="flex flex-1">
-            <Map @point-click="pointClickHandler" />
-        </div>
-        <NavigatorCard v-show="isShowNavigatorCard"
-            :parkName="currentSelectedPark?.name ?? null"
-            :remainingSpace="currentSelectedPark?.remainingSpace ?? null"
-            :price="currentSelectedPark?.price ?? null"
-            :distance="currentSelectedPark?.distance ?? null"
-            :address="currentSelectedPark?.address ?? null"
-            :display="currentSelectedPark?.display || 'navigator_park'"
-            :timePassed="null"
-            :maxTime="null"
-            :leaveEarly="true"
-            show />
+  <div class="h-screen flex flex-col overflow-hidden position-relative">
+    <BaseButton class="button" @click="handleTestClick">123</BaseButton>
+    <div class="flex flex-1">
+      <Map @point-click="pointClickHandler" />
     </div>
+    <NavigatorCard
+      v-show="isShowNavigatorCard"
+      :parkName="currentSelectedPark?.name ?? null"
+      :remainingSpace="currentSelectedPark?.remainingSpace ?? null"
+      :price="currentSelectedPark?.price ?? null"
+      :distance="currentSelectedPark?.distance ?? null"
+      :address="currentSelectedPark?.address ?? null"
+      :display="currentSelectedPark?.display || 'navigator_park'"
+      :timePassed="null"
+      :maxTime="null"
+      :leaveEarly="true"
+      show
+    />
+  </div>
 </template>
