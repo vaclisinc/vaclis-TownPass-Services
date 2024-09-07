596d9021098369fae1fafbb29c77a091d7010e44
WavJaby <wavjaby@gmail.com>
2024-09-08 04:37:21 +0800

    Add data update

--- src/components/organisms/Map.vue
+++ src/components/organisms/Map.vue
@@ -10,94 +10,112 @@ mapboxgl.accessToken =
   'pk.eyJ1IjoiZjc0MTE0NzYwIiwiYSI6ImNtMHJyenV3eTBjOGQyaXNicDFsbXU2YzIifQ.fhoguJDc6TfWAGwn471Hog';
 export default {
   mounted() {
-    const map = new mapboxgl.Map({
+    const map = (this.map = new mapboxgl.Map({
       container: this.$refs.mapContainer,
       style: 'mapbox://styles/mapbox/light-v11', // Replace with your preferred map style
       center: [121.5624999, 25.0325917],
       zoom: 15
+    }));
+
+    map.on('moveend', (e) => {
+      console.log('move end:', e.type);
+      getParkingData();
     });
 
-    let lon = 121.5624999,
-      lat = 25.0325917;
-    axios
-      .get(
-        // `https://127.0.0.1:25569?lon=${lon}&lat=${lat}`
-        `https://api.wavjaby.nckuctf.org:25569?lon=${lon}&lat=${lat}`
-      )
-      .then((i) => i.data)
-      .then((i) => {
-        console.log(i);
-        addPoints(
-          'parkingLot',
-          i.parkingLot.map((i) => ({
-            type: 'Feature',
-            properties: {
-              color: '#ff0000'
-            },
-            geometry: {
-              type: 'Point',
-              coordinates: [i.lon, i.lat]
-            }
-          })),
-          1,
-          3
-        );
-        addPolygon(
-          'parkingGrid',
-          i.parkingGrid.map((i) => ({
-            type: 'Feature',
-            properties: {
-              color: i.available ? '#81b29a' : '#e07a5f'
-            },
-            geometry: {
-              type: 'Polygon',
-              coordinates: [i.wkt]
-            }
-          })),
-          1
-        );
-        // add markers to map
-        i.parkingGrid.forEach((i) => i.available && makeMarker('1', '#26a7ac', 25, [i.lon, i.lat]));
-        i.parkingLot
-          .sort((a, b) => a.carRemainderNum - b.carRemainderNum)
-          .forEach((i) =>
-            makeMarker(
-              i.carRemainderNum,
-              i.carRemainderNum > 0 ? '#693' : '#f20',
-              i.carRemainderNum === 0 ? 20 : Math.min(50 * (i.carRemainderNum / 1000) + 25, 60),
-              [i.lon, i.lat]
-            )
-          );
-      });
-    axios
-      .get(`https://api.wavjaby.nckuctf.org:25569/get_line?lon=${lon}&lat=${lat}`)
-      .then((i) => i.data)
-      .then((i) => {
-        console.log(i);
-        let featuresPath = i.map((i) => ({
-          type: 'Feature',
-          properties: {
-            color: '#ffff00'
-          },
-          geometry: {
-            type: 'LineString',
-            coordinates: i.coordinates
-          }
-        }));
-        addSourceAndLayer('yellowLine', featuresPath, {
-          type: 'line',
-          layout: {
-            'line-join': 'round',
-            'line-cap': 'round'
-          },
-          paint: {
-            'line-color': ['get', 'color'],
-            'line-width': 4
-          }
-        });
-      });
+    let updating = 0;
+    const markers = [];
 
-    this.map = map;
+    getParkingData();
+
+    function getParkingData() {
+      if (updating > 0) return;
+      updating = 2;
+      markers.forEach(i=>i.remove());
+
+      let center = map.getCenter();
+      let lon = center.lng, lat = center.lat;
+      axios
+        .get(
+          // `https://127.0.0.1:25569?lon=${lon}&lat=${lat}`
+          `https://api.wavjaby.nckuctf.org:25569?lon=${lon}&lat=${lat}`
+        )
+        .then((i) => i.data)
+        .then((i) => {
+          console.log(i);
+          addPoints(
+            'parkingLot',
+            i.parkingLot.map((i) => ({
+              type: 'Feature',
+              properties: {
+                color: '#ff0000'
+              },
+              geometry: {
+                type: 'Point',
+                coordinates: [i.lon, i.lat]
+              }
+            })),
+            1,
+            3
+          );
+          addPolygon(
+            'parkingGrid',
+            i.parkingGrid.map((i) => ({
+              type: 'Feature',
+              properties: {
+                color: i.available ? '#81b29a' : '#e07a5f'
+              },
+              geometry: {
+                type: 'Polygon',
+                coordinates: [i.wkt]
+              }
+            })),
+            1
+          );
+          // add markers to map
+          i.parkingGrid.forEach(
+            (i) => i.available && makeMarker('1', '#26a7ac', 25, [i.lon, i.lat])
+          );
+          i.parkingLot
+            .sort((a, b) => a.carRemainderNum - b.carRemainderNum)
+            .forEach((i) =>
+              makeMarker(
+                i.carRemainderNum,
+                i.carRemainderNum > 0 ? '#693' : '#f20',
+                i.carRemainderNum === 0 ? 20 : Math.min(50 * (i.carRemainderNum / 1000) + 25, 60),
+                [i.lon, i.lat]
+              )
+            );
+          updating--;
+        });
+      axios
+        .get(`https://api.wavjaby.nckuctf.org:25569/get_line?lon=${lon}&lat=${lat}`)
+        .then((i) => i.data)
+        .then((i) => {
+          console.log(i);
+          let featuresPath = i.map((i) => ({
+            type: 'Feature',
+            properties: {
+              color: '#ffff00'
+            },
+            geometry: {
+              type: 'LineString',
+              coordinates: i.coordinates
+            }
+          }));
+          addSourceAndLayer('yellowLine', featuresPath, {
+            type: 'line',
+            layout: {
+              'line-join': 'round',
+              'line-cap': 'round'
+            },
+            paint: {
+              'line-color': ['get', 'color'],
+              'line-width': 4
+            }
+          });
+          updating--;
+        });
+    }
 
     function makeMarker(text, color, size, cord) {
       let el = document.createElement('div');
@@ -110,7 +128,7 @@ export default {
       el.append(sp);
 
       // make a marker for each feature and add it to the map
-      new mapboxgl.Marker(el).setLngLat(cord).addTo(map);
+      markers.push(new mapboxgl.Marker(el).setLngLat(cord).addTo(map));
     }
 
     function addSourceAndLayer(id, features, layerConfig) {
