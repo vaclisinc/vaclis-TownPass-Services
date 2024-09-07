62f9856fe52d314ae15b68a3e45d27d0853718f2
nyraa <112930946+nyraa@users.noreply.github.com>
2024-09-07 23:46:51 +0800

    update Parking view

--- src/views/ParkingService.vue
+++ src/views/ParkingService.vue
@@ -2,12 +2,46 @@
 import Map from "@/components/organisms/Map.vue";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import NavigatorCard from "@/components/organisms/NavigatorCard.vue";
+import { ref } from 'vue';
+import { computed } from 'vue';
+
+export type ParkPoint = {
+    name: string;
+    address: string;
+    lat: number;
+    lng: number;
+    remainingSpace: number;
+    price: string;
+    distance: number;
+    display: "navigator_park" | "navigator_yellow_line" | "park_confirm" | "park_timer";
+};
+
+const currentSelectedPark = ref<ParkPoint | null>({
+    name: 'sussy',
+    address: 'safa',
+    lat: 100,
+    lng: 24,
+    remainingSpace: 48763,
+    price: '40圓/小時',
+    distance: 20,
+    display: 'navigator_park'
+});
+
+const handleClick = (point: ParkPoint) => {
+    currentSelectedPark.value = point;
+};
+
+
+const pointClickHandler = ref(handleClick);
+const isShowNavigatorCard = computed(() => currentSelectedPark.value !== null);
+
+// Usage: <Map @point-click="pointClickHandler" />
 </script>
 <template>
     <div class="h-screen flex flex-col overflow-hidden position-relative">
         <div class="flex flex-1">
-            <Map />
+            <Map @point-click="pointClickHandler" />
         </div>
-        <NavigatorCard parkName="foo" remainingSpace="10" price="40圓/小時" distance="10" display="navigator_yellow_line" show />
+        <NavigatorCard v-show="isShowNavigatorCard" :parkName="currentSelectedPark?.name || ''" :remainingSpace="currentSelectedPark?.remainingSpace ?? 0" :price="currentSelectedPark?.price ?? ''" :distance="currentSelectedPark?.distance ?? 0" :display="currentSelectedPark?.display || 'navigator_park'" show />
     </div>
 </template>
